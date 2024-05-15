4431a6437df218d6ce9712a18071361ef8d046be
Oliver-Jerreback-NTI-2C <150040969+Oliver-Jerreback-NTI-2C@users.noreply.github.com>
2024-05-15 13:00:51 +0200

    mer ändringar på hemsidor

--- js/script.js
+++ js/script.js
@@ -21,10 +21,17 @@ const starBilder = [
 ];
 
 const texter = [
-    "Kanske inte den mest bekväma kostymen ganska dålig",
-    "Rätt okej kosytm inte bekvämaste men den är ändå bra",
-    "Jag är väldigt nöjd kanske lite väl ovattentät",
-    "Denna kostymen är perfekt på alla sätt förutom färgen"
+    "Kanske inte den mest bekväma kostymen ganska dålig finns mycket förbättrings potential",
+    "Rätt okej kosytm inte bekvämaste men den är ändå bra för priset hade kanske köpt igen",
+    "Jag är väldigt nöjd med denna kotymen om jag hade ändrat något är det väl nog tätheten",
+    "Denna kostymen är perfekt på alla sätt förutom färgen hade velat ha ett större utbud"
+];
+
+const underrubriker = [
+    "Ganska kass",
+    "Rätt bra",
+    "Bra men gör vattentät",
+    "Mer färger"
 ];
 
 const datum = [
@@ -33,6 +40,21 @@ const datum = [
     "Publicerad: 2022-01-09",
     "Publicerad: 2023-05-17"
 ];
+
+const namn = [
+    "Lars B",
+    "Nils A",
+    "Sven S",
+    "Hans A"
+];
+
+const plats = [
+    "Gotland, SE",
+    "Örebro, SE",
+    "Skåne, SE",
+    "Malmö, SE"
+];
+
 
 var bildspelImgSources = ["img/bildspel/bildspel_1.jpg", "img/bildspel/bildspel_2.jpg", "img/bildspel/bildspel_3.jpg", "img/bildspel/bildspel_4.jpg"];
 var bildspelIndex = 0;
@@ -43,10 +65,7 @@ function newImageRight() {
     } else {
         bildspelIndex++;
     }
-    document.getElementById("img_bildspel").src = bildspelImgSources[bildspelIndex]; 
-    document.getElementById("text").textContent = texter[bildspelIndex]; 
-    document.getElementById("stars").src = starBilder[bildspelIndex]; 
-    document.getElementById("datum").textContent = datum[bildspelIndex]; 
+    updateElements();
 }
 
 function newImageLeft() {
@@ -55,11 +74,22 @@ function newImageLeft() {
     } else {
         bildspelIndex--;
     }
+    updateElements();
+}
+
+function updateElements() {
     document.getElementById("img_bildspel").src = bildspelImgSources[bildspelIndex]; 
-    document.getElementById("text").textContent = texter[bildspelIndex]; 
+    document.getElementById("text_recension").textContent = texter[bildspelIndex]; 
     document.getElementById("stars").src = starBilder[bildspelIndex]; 
     document.getElementById("datum").textContent = datum[bildspelIndex]; 
+    document.getElementById("namn").textContent = namn[bildspelIndex];
+    document.getElementById("plats").textContent = plats[bildspelIndex];
+    document.getElementById("underrubrik").textContent = underrubriker[bildspelIndex];
 }
+
+
+
+
 
 
 arrowLeft.addEventListener('click', newImageLeft);
